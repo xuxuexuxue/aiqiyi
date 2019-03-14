76bc0de4a45a3aa8e79f6f5d138409b3c258ad5a
xuxue <xuxue@qq.com>
2019-03-14 20:01:35 +0800

    对项目进行了配置并集成了日志

--- manage.py
+++ manage.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import logging
 
-app = Flask(__name__)
+from flask import current_app
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from info import db, create_app
 
 
-@app.route('/')
+# 通过指定配置名字创建对应配置的app
+app = create_app('development')
+# Flask-script添加使用命令行
+manager = Manager(app)
+# 数据库迁移
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@app.route('/index')
 def index():
+    # logging.debug("This is a debug log.")
+    current_app.logger.debug('debug')
     return 'index'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
