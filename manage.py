028c4d6597d20574265f0474601a4ef0c6dbfd16
tianyiorg <tianyiorg@gmail.com>
2019-03-17 12:45:51 +0800

    修改index.html中css,js,img的链接地址

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 import logging
 
-from flask import current_app
+from flask import current_app, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import db, create_app
-
 
 # 通过指定配置名字创建对应配置的app
 app = create_app('development')
@@ -15,11 +14,17 @@ Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
-@app.route('/index')
-def index():
-    # logging.debug("This is a debug log.")
-    current_app.logger.debug('debug')
-    return 'index'
+# @app.route('/index')
+# def index():
+#     # logging.debug("This is a debug log.")
+#     current_app.logger.debug('debug')
+#     return 'index'
+
+
+# @app.route('/')
+# @app.route('/index')
+# def movie_index():
+#     return render_template('aiqiyi_movie/index.html')
 
 
 if __name__ == '__main__':
