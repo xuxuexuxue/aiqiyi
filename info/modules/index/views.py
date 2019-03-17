bfa06008b9d62851bdae9ae1e7f01ddd535b9fa4
xuxue <xuxue@qq.com>
2019-03-17 19:13:10 +0800

    提及

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from info import redis_store
 from info.modules.index import index_blu
 
@@ -6,5 +8,6 @@ from info.modules.index import index_blu
 @index_blu.route('/')
 def index():
     # 向redis中保存一个值name xuxue
-    redis_store.set("name", "it")
-    return 'hello'
+    # redis_store.set("name", "it")
+    return render_template("aiqiyi/index.html")
+
